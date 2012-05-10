f04557b21adc5fe0a5710764c1c26a47975d24ed
David Leibovich <ariedov@gmail.com>
2012-05-10 16:19:50 +0300

    fixed sellers

--- apps/mainpage/views.py
+++ apps/mainpage/views.py
@@ -4,14 +4,14 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 def main(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by("-publish_at")
     return render_to_response('main_page.html',{
     	'products':products,},
 	    context_instance = RequestContext(request))
 
 def seller(request, slug):
     seller = get_object_or_404(Seller, slug=slug)
-    products = Product.objects.filter(seller__slug=slug)
+    products = Product.objects.filter(seller__slug=slug).order_by("-publish_at")
     return render_to_response('page.html', {
     	'seller':seller,
 		'products': products,
